92cd326a64433fd77e3507755ab993fee605cbf9
matthova <matthova87@gmail.com>
2017-02-18 21:44:37 -0800

    Added index, edit, update, and destroy controllers and views

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -9,6 +9,7 @@ module.exports = {
   new: (req, res) => {
     res.view();
   },
+
   create: (req, res, next) => {
     // Create a User with the params sent from
     // the sign-up form --> new.ejs
@@ -21,9 +22,10 @@ module.exports = {
         return res.redirect('/user/new');
       }
 
-      res.json(user);
+      res.redirect('/user/show/' + user.id);
     });
   },
+
   show: (req, res, next) => {
     User.findOne(req.param('id'), function foundUser (err, user) {
       if (err) {
@@ -34,6 +36,59 @@ module.exports = {
       }
 
       res.view({ user });
+    });
+  },
+
+  index: (req, res, next) => {
+    // Get an array of all users in the User collection(e.g. table)
+    User.find(function foundUsers (err, users) {
+      if (err) {
+        return next(err);
+      }
+      res.view({ users });
+    });
+  },
+
+  edit: (req, res, next) => {
+    // Find the user from the id passed in via params
+    User.findOne(req.param('id'), function foundUser (err, user) {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return next('User doesn\'t exist.');
+      }
+
+      res.view({ user });
+    });
+  },
+
+  update: (req, res, next) => {
+    User.update(req.param('id'), req.params.all(), function userUpdated (err) {
+      if (err) {
+        return res.redirect('/user/edit/' + req.param('id'));
+      }
+
+      res.redirect('/user/show/' + req.param('id'));
+    });
+  },
+
+  destroy: (req, res, next) => {
+    User.findOne(req.param('id'), function foundUser (err, user) {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return next('User doesn\'t exist.');
+      }
+
+      User.destroy(req.param('id'), function userDestroyed(err) {
+        if (err) {
+          return next(err);
+        }
+      });
+
+      res.redirect('/user');
     });
   }
 };
