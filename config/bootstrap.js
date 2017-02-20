5fb4e82eecc214257473a6f6ac92e2fc543f214e
matthova <matthova87@gmail.com>
2017-02-19 20:56:06 -0800

    Adding an admin user if there is none

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,9 +9,46 @@
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
-module.exports.bootstrap = function(cb) {
+const bcrypt = require('bcrypt');
 
+function initializeAdminUser(cb) {
+  User.find(function foundUsers(err, users) {
+    if (err) {
+      throw new Error(err);
+    }
+    if (users.length > 0) {
+      return cb();
+    }
+
+    const adminProperties = {
+      name: 'me',
+      title: 'person',
+      email: 'me@me.com',
+      // What keeps anyone from creating an admin user via post?
+      admin: true,
+      password: 'password',
+      confirmation: 'password',
+    };
+
+    bcrypt.hash('password', 10, function passwordEncrypted(err, encryptedPassword) {
+      if (err) {
+        return next(err);
+      }
+
+      adminProperties.encryptedPassword = encryptedPassword;
+
+      User.create(adminProperties, function userCreated(err, user) {
+        if (err) {
+          throw new Error(JSON.stringify(err));
+        }
+        cb();
+      });
+    });
+  });
+}
+
+module.exports.bootstrap = function(cb) {
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  initializeAdminUser(cb);
 };
